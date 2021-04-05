c0ec5927177979d31a6d5ba5290ecfb71284439e
pavankalyan0105 <pavankalyan0105@gmail.com>
2021-04-06 05:06:35 +0530

    added auth.js in controllers

---
+++ projbackend/controllers/auth.js
@@ -0,0 +1,7 @@
+
+
+exports.signout = (req , res) =>{
+    res.json({
+        message: 'user signout'
+    })
+}
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/signout' , (req , res) => {
-    res.send("user signout")
-})
+const {signout} = require("../controllers/auth")
+
+router.get('/signout' , signout)
 
 
 router.get('/signin' , (req , res)=>{
