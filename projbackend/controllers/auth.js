35538ca0513e0592dd38040460aeee2744217de7
pavankalyan0105 <pavankalyan0105@gmail.com>
2021-05-07 11:55:20 +0530

    checking the email,name , password before moving onto save

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,23 +1,37 @@
 
 const User = require('../models/user')
+const { check, validationResult } = require('express-validator');
+
 
 
 exports.signup = (req , res) =>{
+
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error : errors.array()[0].param + " " + errors.array()[0].msg,
+
+        });
+    }   
+    
+    
     // console.log("REQ BODY" , req.body);
     const user = new User(req.body);
 
-    user.save( (err , data)=>{
+    user.save( (err , user)=>{
         if(err){
             return res.status(400).json({
-                err: "Not able save user in DB"
+                err: "Not able to save user"
             })
         }
-        res.json({
-            name:user.name,
-            email:user.email,
-        });
-    })
 
+            res.json({
+                "name": user.name,
+                "email": user.email    
+        })
+    });
 }
 
 
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,4 +1,5 @@
 var express = require('express')
+const { check, validationResult } = require('express-validator');
 var router = express.Router()
 
 const {signout , signup } = require("../controllers/auth")
@@ -6,7 +7,11 @@ const {signout , signup } = require("../controllers/auth")
 // router.get('/signout' , signout)
 
 
-router.post('/signup'  ,signup)
+router.post('/signup'  ,[
+    check("name").isLength({min : 3}).withMessage('Name is mandatory') ,
+    check('email').isEmail().withMessage('Email is required') ,
+    check("password").isLength({min : 8}).withMessage('must be at  5 chars long'),
+] , signup)
 
 
 
